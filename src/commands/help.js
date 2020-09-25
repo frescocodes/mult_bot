7346698e17f7b350dd2360aa0c005f1f6c2095b2
unknown <iggylow1@gmail.com>
2020-09-25 05:09:53 -0500

    setup

---
+++ src/commands/help.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const commandFiles = fs.readdirSync("./src/commands").filter((file) => file);
+
+console.log("commandFiles", commandFiles);
+
+module.exports = {
+  name: "help",
+  description: "List of valid commands",
+  execute(message, args) {
+    message.reply(
+      "\nHere is a list of commands:" +
+        "\n```" +
+        commandFiles.map((element) => `\n${element}`) +
+        "\n```"
+    );
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const fs = require("fs");
+const { prefix } = require("../config.json");
+// import Discord
+const Discord = require("discord.js");
+// instantiate new instance of Discord.Client()
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./src/commands/")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  // set a new item in Collection
+  // with key as command name and value as exported module
+  client.commands.set(command.name, command);
+}
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// listen for messages
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) {
+    return;
+  } else {
+    try {
+      client.commands.get(command).execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply("There was an error executing that");
+    }
+  }
+});
+
+client.login(process.env.SB_TOKEN);
