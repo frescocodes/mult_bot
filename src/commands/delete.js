4ce0b3695fe7f37b66f26a83f0029cb2eeda5edd
unknown <iggylow1@gmail.com>
2020-11-08 00:10:56 -0600

    feat: bulk deletion command

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -1,7 +1,15 @@
 module.exports = {
   name: "delete",
   description: "Deletes last n messages",
-  execute(message, args) {
-    console.log(message);
+  async execute(message, args) {
+    if (parseInt(args[0]) < 2) {
+      message.reply("Please state an amount of at least 2");
+    } else {
+      // delete original command
+      message.delete();
+      // get n messages
+      const fetched = await message.channel.messages.fetch({ limit: args[0] });
+      message.channel.bulkDelete(fetched);
+    }
   },
 };
